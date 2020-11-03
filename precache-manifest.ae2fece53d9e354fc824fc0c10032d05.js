4b930f19e5c1798213fa17962a6d6fb09c1dcecd
simon-svg <simonkarapetyan605@gmail.com>
2020-11-03 13:31:36 +0400

    Updates

---
+++ precache-manifest.ae2fece53d9e354fc824fc0c10032d05.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c8387dcb220676ca925954d96f8c27",
+    "url": "/SmartR/index.html"
+  },
+  {
+    "revision": "25d4dd905adbb4e0f52e",
+    "url": "/SmartR/static/css/main.0e493e6c.chunk.css"
+  },
+  {
+    "revision": "1296961e7a0374c37aca",
+    "url": "/SmartR/static/js/2.674ce841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SmartR/static/js/2.674ce841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d4dd905adbb4e0f52e",
+    "url": "/SmartR/static/js/main.f32a5d0d.chunk.js"
+  },
+  {
+    "revision": "085bb2f9a14e542a64fc",
+    "url": "/SmartR/static/js/runtime-main.92b0d2e1.js"
+  },
+  {
+    "revision": "5487bea91cef82008fdc6e874b4c5c86",
+    "url": "/SmartR/static/media/OpenSans-Bold.5487bea9.eot"
+  },
+  {
+    "revision": "759ea19bccf8a5ca50a58e474aced157",
+    "url": "/SmartR/static/media/OpenSans-Bold.759ea19b.woff"
+  },
+  {
+    "revision": "93fe8f3415688d16bb947e2c96ea380e",
+    "url": "/SmartR/static/media/OpenSans-Bold.93fe8f34.woff2"
+  },
+  {
+    "revision": "ab38fda09644c992dc09754c3cc78201",
+    "url": "/SmartR/static/media/OpenSans-Bold.ab38fda0.ttf"
+  },
+  {
+    "revision": "0a96d2322aa76287314064b8f9b55466",
+    "url": "/SmartR/static/media/OpenSans-BoldItalic.0a96d232.woff"
+  },
+  {
+    "revision": "183f1aa2eb319f83e073f83f72a94dbb",
+    "url": "/SmartR/static/media/OpenSans-BoldItalic.183f1aa2.woff2"
+  },
+  {
+    "revision": "30dd88ede7a982a2e92f0d9420dfb07d",
+    "url": "/SmartR/static/media/OpenSans-BoldItalic.30dd88ed.eot"
+  },
+  {
+    "revision": "ad5795be528d8256833a01e9b7a0f5b6",
+    "url": "/SmartR/static/media/OpenSans-BoldItalic.ad5795be.ttf"
+  },
+  {
+    "revision": "1b2c3c4788d5f305789b49f7d38e85c5",
+    "url": "/SmartR/static/media/OpenSans-ExtraBold.1b2c3c47.woff2"
+  },
+  {
+    "revision": "3f760f718edb65cd95c6da14d6ab1bb8",
+    "url": "/SmartR/static/media/OpenSans-ExtraBold.3f760f71.woff"
+  },
+  {
+    "revision": "b279b074e3f149694a8181dd6225bffc",
+    "url": "/SmartR/static/media/OpenSans-ExtraBold.b279b074.eot"
+  },
+  {
+    "revision": "c9e437c181cb1c201188c2793fbc496f",
+    "url": "/SmartR/static/media/OpenSans-ExtraBold.c9e437c1.ttf"
+  },
+  {
+    "revision": "3548064077a9426230de0fe377df422e",
+    "url": "/SmartR/static/media/OpenSans-ExtraBoldItalic.35480640.eot"
+  },
+  {
+    "revision": "d69a748730ffaa1494fbd42d8b25233a",
+    "url": "/SmartR/static/media/OpenSans-ExtraBoldItalic.d69a7487.woff2"
+  },
+  {
+    "revision": "edbbf5335fcb1e037d0ed16c17364c16",
+    "url": "/SmartR/static/media/OpenSans-ExtraBoldItalic.edbbf533.woff"
+  },
+  {
+    "revision": "f093928db8789603f7913b5647fddddb",
+    "url": "/SmartR/static/media/OpenSans-ExtraBoldItalic.f093928d.ttf"
+  },
+  {
+    "revision": "052b69c9c418c621fbd978d33e975b92",
+    "url": "/SmartR/static/media/OpenSans-Italic.052b69c9.woff"
+  },
+  {
+    "revision": "1ec1b222862acc96228ef185baab971b",
+    "url": "/SmartR/static/media/OpenSans-Italic.1ec1b222.eot"
+  },
+  {
+    "revision": "a709ab6006407a103054d8a64459594b",
+    "url": "/SmartR/static/media/OpenSans-Italic.a709ab60.ttf"
+  },
+  {
+    "revision": "c27ebc192b3fe6167aa988bc77c97139",
+    "url": "/SmartR/static/media/OpenSans-Italic.c27ebc19.woff2"
+  },
+  {
+    "revision": "559e978f49dc58dbe4213c714d308483",
+    "url": "/SmartR/static/media/OpenSans-Light.559e978f.woff2"
+  },
+  {
+    "revision": "b270c40ae5ed0192f44df55ad6f1ea25",
+    "url": "/SmartR/static/media/OpenSans-Light.b270c40a.ttf"
+  },
+  {
+    "revision": "c002a58fc48427af4884fcce2d5c03ee",
+    "url": "/SmartR/static/media/OpenSans-Light.c002a58f.woff"
+  },
+  {
+    "revision": "d8ba00bc265c4a0554703cf89fc35e6e",
+    "url": "/SmartR/static/media/OpenSans-Light.d8ba00bc.eot"
+  },
+  {
+    "revision": "5a3ac4d0297da9f2b75c4fe965e004cb",
+    "url": "/SmartR/static/media/OpenSans-LightItalic.5a3ac4d0.ttf"
+  },
+  {
+    "revision": "c2e4b54642e6a4a47da11d91e6edbb54",
+    "url": "/SmartR/static/media/OpenSans-LightItalic.c2e4b546.eot"
+  },
+  {
+    "revision": "d821368c1b1a2f95186faf4a99d30e89",
+    "url": "/SmartR/static/media/OpenSans-LightItalic.d821368c.woff2"
+  },
+  {
+    "revision": "f98d42be17e95311f1f82823fb3b6a77",
+    "url": "/SmartR/static/media/OpenSans-LightItalic.f98d42be.woff"
+  },
+  {
+    "revision": "38ca9e2302c8793c93f854e8595eed78",
+    "url": "/SmartR/static/media/OpenSans-Regular.38ca9e23.eot"
+  },
+  {
+    "revision": "5d5735e57127db2f7a2ad879fc6056b8",
+    "url": "/SmartR/static/media/OpenSans-Regular.5d5735e5.woff2"
+  },
+  {
+    "revision": "6fde2eb6728eb97fd914dcd0641d332b",
+    "url": "/SmartR/static/media/OpenSans-Regular.6fde2eb6.woff"
+  },
+  {
+    "revision": "8df00d6176ddae387d9431d3307b3e62",
+    "url": "/SmartR/static/media/OpenSans-Regular.8df00d61.ttf"
+  },
+  {
+    "revision": "7106bb83a0e767e12659de9cfba3926d",
+    "url": "/SmartR/static/media/OpenSans-SemiBold.7106bb83.woff2"
+  },
+  {
+    "revision": "e0d5280b186197bd2a77b9babfe5506d",
+    "url": "/SmartR/static/media/OpenSans-SemiBold.e0d5280b.eot"
+  },
+  {
+    "revision": "f28ed2e40f2a6955ddc5dd3f5088a5a2",
+    "url": "/SmartR/static/media/OpenSans-SemiBold.f28ed2e4.ttf"
+  },
+  {
+    "revision": "fe241c6c4f5a190e821b7c03e337d756",
+    "url": "/SmartR/static/media/OpenSans-SemiBold.fe241c6c.woff"
+  },
+  {
+    "revision": "02669c88881b1577a54e9e2d76f25566",
+    "url": "/SmartR/static/media/OpenSans-SemiBoldItalic.02669c88.woff"
+  },
+  {
+    "revision": "0d92fc08a8a83e6b2956bf04e7dd0a82",
+    "url": "/SmartR/static/media/OpenSans-SemiBoldItalic.0d92fc08.eot"
+  },
+  {
+    "revision": "3dc654f4cee24692f6441cab8c155df0",
+    "url": "/SmartR/static/media/OpenSans-SemiBoldItalic.3dc654f4.ttf"
+  },
+  {
+    "revision": "da6f476d3784fdfb4b3325c491418c69",
+    "url": "/SmartR/static/media/OpenSans-SemiBoldItalic.da6f476d.woff2"
+  },
+  {
+    "revision": "0086c0005c802f964bf16d2927e53b62",
+    "url": "/SmartR/static/media/apple.0086c000.png"
+  }
+]);
